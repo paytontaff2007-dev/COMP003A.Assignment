8d34174996044a860056cffe0b9ee4e7cc5168b1
PaytonTaff <paytontaff2007@gmail.com>
2026-01-14 21:10:18 -0800

    Items 11-20

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -15,6 +15,40 @@
 			Console.WriteLine("8. I play on a Xbox series X");
 			Console.WriteLine("9. I like yugioh");
 			Console.WriteLine("10. I like Magic The Gathering");
+			Console.WriteLine("11. My first console was the nintendo 64");
+			Console.WriteLine("12. Dragon ball super is my favorite anime");
+			Console.WriteLine("13. My favorite food is tacos");
+			Console.WriteLine("14. Greninja is my favorite pokemon");
+			Console.WriteLine("15. Godzilla is my favorite movie");
+			Console.WriteLine("16. Transformers is my favorite tv show");
+			Console.WriteLine("17. Math is my least favorite class");
+			Console.WriteLine("18. English is an okay class to me");
+			Console.WriteLine("19. I am not great at coding but am trying to learn");
+			Console.WriteLine("20. PC's are intresting and very helpfull"); 
+			Console.WriteLine(""); 
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
 			Console.WriteLine("");
 			Console.WriteLine("");
 			Console.WriteLine("");
@@ -25,77 +59,45 @@
 			Console.WriteLine("");
 			Console.WriteLine("");
 			Console.WriteLine(""); 
-			Console.WriteLine(""); 
-			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
 			Console.WriteLine("");
 			Console.WriteLine(""); 
 			Console.WriteLine("");
-			Console.WriteLine(""); 
 			Console.WriteLine("");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
 
 
 
