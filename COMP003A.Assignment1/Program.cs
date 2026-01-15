45e93912daebbfe3df126ac5538912271e2c2de1
PaytonTaff <paytontaff2007@gmail.com>
2026-01-14 19:38:49 -0800

    code cleanup

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,19 +1,10 @@
-﻿using System.Reflection;
-using Microsoft.VisualBasic;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace COMP003A.Assignment1
+﻿namespace COMP003A.Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-
-
-
         }
-
     }
 }
-
