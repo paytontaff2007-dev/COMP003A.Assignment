ce9d24c46a798977068542fdad82e6afa3e71229
PaytonTaff <paytontaff2007@gmail.com>
2026-01-14 20:58:51 -0800

    Items 1-10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -4,7 +4,108 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I am 18 years old");
+            Console.WriteLine("2. My favorite video game is Digimon");
+            Console.WriteLine("3. I like music");
+            Console.WriteLine("4. I am going into Information Technology");
+            Console.WriteLine("5. I am a big fan of history");
+			Console.WriteLine("6. My favorite soda is pepsi");
+			Console.WriteLine("7. I like to draw");
+			Console.WriteLine("8. I play on a Xbox series X");
+			Console.WriteLine("9. I like yugioh");
+			Console.WriteLine("10. I like Magic The Gathering");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine(""); 
+			Console.WriteLine(""); 
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+			Console.WriteLine(""); 
+			Console.WriteLine("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}
     }
 }
