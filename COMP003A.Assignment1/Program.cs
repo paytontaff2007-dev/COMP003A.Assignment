a5392a00e01a1749533cb9ab4259ec126d6afbfa
payto <payto@PAYTON-TAFF>
2026-01-14 19:29:14 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+        }
+
+    }
+}
+
